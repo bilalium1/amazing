afc9742e895d1a49fda5ad9e3d87d8b99f33125a
b11 <lemrabetbilal90@gmail.com>
2026-02-11 01:21:29 +0100

    jj

--- display.py
+++ display.py
@@ -1,8 +1,44 @@
 from mlx import Mlx
-import sys
+import os
+import time
 import random
 
-matrix = [[random.randint(0, 15) for _ in range(5)] for _ in range(5)]
+ESC = 65307  # X11 ESC KEYCODE
+
+matrix = [[random.randint(0, 15) for _ in range(10)] for _ in range(10)]
+
+
+class block:
+    def __init__(self, info, size, offset, color):
+        self.i = info
+        self.s = size
+        self.o = offset
+        self.c = color
+    
+    def draw(self):
+        if (self.i & 1):  # EAST
+            for y in range(self.o[1], self.o[1] + self.s):
+                mlx.mlx_pixel_put(mlx_ptr, win_ptr, self.o[0] + self.s, y, self.c)
+        if (self.i & 2):  # NORTH
+            for x in range(self.o[0], self.o[0] + self.s):
+                mlx.mlx_pixel_put(mlx_ptr, win_ptr, x, self.o[1], self.c)
+        if (self.i & 4):  # WEST
+            for y in range(self.o[1], self.o[1] + self.s):
+                mlx.mlx_pixel_put(mlx_ptr, win_ptr, self.o[0], y, self.c)
+        if (self.i & 8):  # SOUTH
+            for x in range(self.o[0], self.o[0] + self.s):
+                mlx.mlx_pixel_put(mlx_ptr, win_ptr, x, self.o[1] + self.s, self.c)
+
+    def animate(self):
+        for i in range(self.o[0], self.o[0] + self.s):
+            for j in range(self.o[1], self.o[1] + self.s):
+                mlx.mlx_pixel_put(mlx_ptr, win_ptr, i, j, self.c)
+            time.sleep((0.05) / self.s)
+
+    def clear(self):
+        for i in range(self.o[0], self.o[0] + self.s):
+            for j in range(self.o[1], self.o[1] + self.s):
+                mlx.mlx_pixel_put(mlx_ptr, win_ptr, i, j, j + i)
 
 for row in matrix:
     print(row)
@@ -11,39 +47,30 @@ mlx = Mlx()
 mlx_ptr = mlx.mlx_init()
 if not mlx_ptr:
     print("MLX init failed")
-    sys.exit(1)
+    os._exit(1)
 
 win_ptr = mlx.mlx_new_window(mlx_ptr, 1920, 1080, "MLX Test")
 
-# Draw some pixels
-for x in range(0, 200):
-    for y in range(50, 150):
-        mlx.mlx_pixel_put(mlx_ptr, win_ptr, x, y, 0x0000FF)  # green square
+for i in range(0, 9):
+    for j in range(0, 9):
+        b = block(matrix[i][j], 25, (j * 25, i * 25), 0xFFFFFF)
+        b.animate()
+        b.clear()
+        b.draw()
 
 # Draw a string
-mlx.mlx_string_put(mlx_ptr, win_ptr, 150, 20, 0xFF0000, "Hello MLX")
-
+mlx.mlx_string_put(mlx_ptr, win_ptr, 300, 20, 0xFF0000, "MAZE ")
 
 def close_window(keycode, param):
-    mlx.mlx_destroy_window(mlx_ptr, win_ptr)
-    sys.exit(0)
+    if keycode == ESC:
+        mlx.mlx_destroy_window(mlx_ptr, win_ptr)
+        os._exit(0)
 
 
-mlx.mlx_hook(win_ptr, 17, 0, close_window, None)
+mlx.mlx_key_hook(win_ptr, close_window, None)
 mlx.mlx_loop(mlx_ptr)
 
-# Start loop onc
+int(input("hello"))
 
-
-class block:
-    def __init__(self, info, size, offset, color):
-        self.info = info
-        self.size = size
-        self.offset = offset
-        self.color = color
-    
-    def draw(self):
-        for x in range(self.offset[0], self.offset[0] + self.size):
-            for y in range(self.offset[1], self.offset[1] + self.size):
-                mlx.mlx_pixel_put(mlx_ptr, win_ptr, x, y, 255)
-
+# swne
+# 0000
